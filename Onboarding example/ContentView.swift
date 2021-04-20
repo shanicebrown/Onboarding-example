49883514b0e746aeecd0457e34a27fadc7cd6390
Shanice Brown <s.mariebrown10@gmail.com>
2021-04-20 08:55:47 -0500

    Initial Commit

---
+++ Onboarding example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding example
+//
+//  Created by Shanice Brown (WHA) on 4/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Onboarding example/Onboarding_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_exampleApp.swift
+//  Onboarding example
+//
+//  Created by Shanice Brown (WHA) on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
