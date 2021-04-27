16ed7e9f8f0245f2da22a18af1cc9f7c6705ab8a
Shanice Brown <s.mariebrown10@gmail.com>
2021-04-27 17:54:14 -0500

    This is the Onboarding
    This is the Onboarding

--- Onboarding example/ContentView.swift
+++ Onboarding example/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("currentPage") var currentPage = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+     
+        if currentPage > totalPages{
+            Home()
+        }
+        else{
+            WalkthroughScreen()
+        }
     }
 }
 
@@ -19,3 +25,174 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Home Page....
+struct Home: View {
+    
+    var body: some View{
+        
+        Text("The Barter Shop")
+            .font(.title)
+            .fontWeight(.heavy)
+    }
+}
+
+// WalkThrough SCreen....
+
+struct WalkthroughScreen: View {
+    
+    @AppStorage("currentPage") var currentPage = 1
+    
+    var body: some View{
+        
+        // For Slide Animation...
+        
+        ZStack{
+            
+            // Changing Between Views....
+            
+            if currentPage == 1{
+                ScreenView(image: "Illustration 3", title: "Step 1", detail: "", bgColor: Color("color1"))
+                    .transition(.scale)
+            }
+            if currentPage == 2{
+            
+                ScreenView(image: "Illustration 4", title: "Step 2", detail: "", bgColor: Color("color2"))
+                    .transition(.scale)
+            }
+            
+            if currentPage == 3{
+                
+                ScreenView(image: "Illustration 3", title: "Step 3", detail: "", bgColor: Color("color3"))
+                    .transition(.scale)
+            }
+            
+        }
+        .overlay(
+        
+            // Button...
+            Button(action: {
+                // changing views...
+                withAnimation(.easeInOut){
+                    
+                    // checking....
+                    if currentPage <= totalPages{
+                        currentPage += 1
+                    }
+                }
+            }, label: {
+                
+                Image(systemName: "chevron.right")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(.black)
+                    .frame(width: 60, height: 60)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                // Circlular Slider...
+                    .overlay(
+                    
+                        ZStack{
+                            
+                            Circle()
+                                .stroke(Color.black.opacity(0.04),lineWidth: 4)
+                                
+                            
+                            Circle()
+                                .trim(from: 0, to: CGFloat(currentPage) / CGFloat(totalPages))
+                                .stroke(Color.white,lineWidth: 4)
+                                .rotationEffect(.init(degrees: -90))
+                        }
+                        .padding(-15)
+                    )
+            })
+            .padding(.bottom,20)
+            
+            ,alignment: .bottom
+        )
+    }
+}
+
+struct ScreenView: View {
+    
+    var image: String
+    var title: String
+    var detail: String
+    var bgColor: Color
+    
+    @AppStorage("currentPage") var currentPage = 1
+    
+    var body: some View {
+        VStack(spacing: 20){
+            
+            HStack{
+                
+                // Showing it only for first Page...
+                if currentPage == 1{
+                    Text("The Barter Shop")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                   
+                        // Letter Spacing...
+                        .kerning(1.4)
+                }
+                else{
+                    // Back Button...
+                    Button(action: {
+                        withAnimation(.easeInOut){
+                            currentPage -= 1
+                        }
+                    }, label: {
+                        
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.white)
+                            .padding(.vertical,10)
+                            .padding(.horizontal)
+                            .background(Color.black.opacity(0.4))
+                            .cornerRadius(10)
+                    })
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation(.easeInOut){
+                        currentPage = 4
+                    }
+                }, label: {
+                    Text("Skip")
+                        .fontWeight(.semibold)
+                        .kerning(1.2)
+                })
+            }
+            
+            .foregroundColor(.black)
+            .padding()
+            
+            Spacer(minLength: 0)
+            
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding(.top)
+            
+            // Change with your Own Thing....
+            Text("Lorem ipsum is dummy text used in laying out print, graphic or web designs.")
+                .fontWeight(.semibold)
+                .kerning(1.3)
+                .multilineTextAlignment(.center)
+            
+            // Minimum Spacing When Phone is reducing...
+            
+            Spacer(minLength: 120)
+        }
+        .background(bgColor.cornerRadius(10).ignoresSafeArea())
+    }
+}
+
+// total Pages...
+var totalPages = 3
